befa6f5eeca4dc6774b1cf6fed8602e2cb32ae0a
Jonathan Obert <jonathan.obert@doku.com>
2024-08-22 11:53:19 +0700

    add v1 payment notification converter

---
+++ src/main/java/com/doku/sdk/dokujavalibrary/common/ConverterUtils.java
@@ -0,0 +1,28 @@
+package com.doku.sdk.dokujavalibrary.common;
+
+import com.doku.sdk.dokujavalibrary.exception.BadRequestException;
+
+import java.net.URLEncoder;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class ConverterUtils {
+
+    public static String toFormData(Map<String, String> request) {
+        StringBuilder v1FormData = new StringBuilder();
+        for (Map.Entry<String, String> entry : request.entrySet()) {
+            if (v1FormData.length() > 0) {
+                v1FormData.append("&");
+            }
+
+            try {
+                v1FormData.append(URLEncoder.encode(entry.getKey(), StandardCharsets.UTF_8.toString()))
+                        .append("=")
+                        .append(URLEncoder.encode(entry.getValue(), StandardCharsets.UTF_8.toString()));
+            } catch (Exception e) {
+                throw new BadRequestException("", e.getMessage());
+            }
+        }
+        return v1FormData.toString();
+    }
+}
